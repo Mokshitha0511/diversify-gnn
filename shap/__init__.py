ffccbe8204fb910123844b5e796a254a805a99cd
Mokshitha0511 <mokshithakoneru@gmail.com>
2025-07-01 13:51:12 +0530

    Delete shap directory

--- shap/__init__.py
+++
@@ -1,8 +0,0 @@
-# shap/__init__.py
-
-"""
-This file marks the 'shap' directory as a Python package.
-You can also use this to import utilities from shap_utils.py directly.
-"""
-
-from .shap_utils import *
