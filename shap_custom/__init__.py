d3041c1e728aa6fa80109e74666f6e769d58f96d
Mokshitha0511 <mokshithakoneru@gmail.com>
2025-07-01 13:52:21 +0530

    Create __init__.py

---
+++ shap_custom/__init__.py
@@ -0,0 +1,8 @@
+# shap_custom/__init__.py
+
+"""
+This file marks the 'shap' directory as a Python package.
+You can also use this to import utilities from shap_utils.py directly.
+"""
+
+from .shap_utils import *
