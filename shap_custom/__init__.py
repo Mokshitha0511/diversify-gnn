ce8480e120765fa2cf3b50b2052496d0685c7d40
Mokshitha0511 <mokshithakoneru@gmail.com>
2025-07-01 15:01:31 +0530

    Update __init__.py

--- shap_custom/__init__.py
+++ shap_custom/__init__.py
@@ -5,4 +5,7 @@ This file marks the 'shap' directory as a Python package.
 You can also use this to import utilities from shap_utils.py directly.
 """
 
-from .shap_utils import *
+from .shap4D import *           # import all public functions/classes from shap4D.py
+from .shap_evaluation import *  # import all from shap_evaluation.py
+from .shap_utils import *       # import all from shap_utils.py
+from .shap_utils_extended import *  # import all from shap_utils_extended.py
